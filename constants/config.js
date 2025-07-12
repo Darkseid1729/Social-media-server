a4fa35c954f35fc0efe32662d545296b27ada32f
Darkseid1729 <134936075+Darkseid1729@users.noreply.github.com>
2025-07-13 02:32:58 +0530

    Update config.js

--- constants/config.js
+++ constants/config.js
@@ -1,9 +1,6 @@
 const corsOptions = {
   origin: [
-    "http://localhost:5173",
-    "http://localhost:4173",
     process.env.CLIENT_URL,
-        "https://social-media-client-dc47xqmno-oryh143-gmailcoms-projects.vercel.app",
 
   ],
   methods: ["GET", "POST", "PUT", "DELETE"],
